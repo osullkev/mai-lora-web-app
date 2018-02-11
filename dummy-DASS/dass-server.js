82db2a860be86ad30449a406fcdd69d137b08a20
Kevin O Sullivan <osullkev@gmail.com>
2018-02-11 17:35:02 +0000

    dass-server.js now accepts an input message and will post it to the LoRa application. This will emulate uplink messages coming from the node ..
    * Added prettier formatting

--- dummy-DASS/dass-server.js
+++ dummy-DASS/dass-server.js
@@ -48,7 +48,9 @@ var express = require('express');
 var basicAuth = require('basic-auth');
 //var rest = require('./doRest.js');
 var u = require('url');
-
+var prompt = require('prompt');
+var httpRequest = require('request');
+require('colors');
 
 var log = function (o) {
     if (debug) {
@@ -59,132 +61,153 @@ var log = function (o) {
 // allow to do HTTPS to self signed servers.
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-
-
-
-
-
-    /* 
-     *
-     *  APPLICATION SERVER 
-     *  -> creating a http server that forwards all received payloads to a server
-     *
-     */
-
-
-
-    var cfg;
-    var userName;
-
     // Application server declaration, using express.js
-    var appServer = express();
-    var httpServer = http.createServer(appServer).listen(appSrvrPort, function () {
-        var host = httpServer.address().address;
-        var port = httpServer.address().port;
-        console.log('Starting server listening on port ' + port);
+var appServer = express();
+var httpServer = http.createServer(appServer).listen(appSrvrPort, function () {
+    var host = httpServer.address().address;
+    var port = httpServer.address().port;
+    console.log('Starting server listening on port ' + port);
+});
 
-        // Registration for PUSH
-        //startPush(null, host);
 
+
+// PUT management from DASS
+appServer.put('/*', function (req, res) {
+
+    log("--------------");
+    log("Received Message put on URL : " + req.url);
+    var url = u.parse(req.url);
+    log(url);
+
+    req.setEncoding('utf8');
+
+    var payload = '';
+    req.on('data', function (chunk) {
+        payload += chunk;
     });
 
-
-
-    // PUT management from DASS
-    appServer.put('/*', function (req, res) {
-
-        log("--------------");
-        log("Received Message put on URL : " + req.url);
-        var url = u.parse(req.url);
-        log(url);
-
-        req.setEncoding('utf8');
-
-        var payload = '';
-        req.on('data', function (chunk) {
-            payload += chunk;
-        });
-
-        // When the message body is available we can do the query.
-        req.on('end', function () {
-            try {
-                var obj = JSON.parse(payload);
-                if (obj.dataFrame) {
-                    obj.dataFrame = new Buffer(obj.dataFrame, 'Base64').toString("hex");
-                }
-
-                if (obj.timestamp) {
-                    if (obj.timestamp.toUpperCase().search("Z") >= 0) {
-                        obj.timestamp = new Date(obj.timestamp).toJSON();
-                    } else {
-                        obj.timestamp = new Date(obj.timestamp + "Z").toJSON();
-                    }
-                }
-
-
-                if (obj.gtw_json) {
-                    obj.gtw_json = JSON.parse(obj.gtw_json);
-                }
-
-                obj.type = req.url.replace("/rest/callback/", "");
-
-
-
-                log(obj);
-				
-				//Logging out the payload object
-				console.log(obj);
-
-            } catch (ex) {
-                console.log("error in payload - no json object found");
-                console.log("received payload: " + payload);
+    // When the message body is available we can do the query.
+    req.on('end', function () {
+        try {
+            var obj = JSON.parse(payload);
+            if (obj.dataFrame) {
+                obj.dataFrame = new Buffer(obj.dataFrame, 'Base64').toString("hex");
             }
 
-        });
-        res.status(202).json({}); // Returning empty body & 202 in order to keep payloads on the DASS
-    });
-
-
-
-
-    // POST managament from DASS 
-    appServer.post('/*', function (req, res) {
-
-        log("--------------");
-        log("Received Message posted on URL : " + req.url);
-
-        req.setEncoding('utf8');
-
-        var payload = '';
-        req.on('data', function (chunk) {
-            payload += chunk;
-        });
-
-        // When the message body is available we can do the query.
-        req.on('end', function () {
-            try {
-                var obj = JSON.parse(payload);
-                if (obj.dataFrame) {
-                    obj.dataFrame = new Buffer(obj.dataFrame, 'Base64').toString("hex");
-                }
-                if (obj.timestamp) {
+            if (obj.timestamp) {
+                if (obj.timestamp.toUpperCase().search("Z") >= 0) {
                     obj.timestamp = new Date(obj.timestamp).toJSON();
+                } else {
+                    obj.timestamp = new Date(obj.timestamp + "Z").toJSON();
                 }
-
-                if (obj.gtw_json) {
-                    obj.gtw_json = JSON.parse(obj.gtw_json);
-                }
-
-                obj.type = req.url.replace("/rest/callback/", "");
-                
-				//Logging out the payload object
-				console.log(obj);
-
-
-            } catch (ex) {
-                log("error in payload - no json object found");
-                log(payload);
             }
-        });
-        res.status(202).json({}); // Returning empty body & 202 in order to keep payloads on the DASS 
+
+
+            if (obj.gtw_json) {
+                obj.gtw_json = JSON.parse(obj.gtw_json);
+            }
+
+            obj.type = req.url.replace("/rest/callback/", "");
+            log(obj);
+				
+     		//Logging out the payload object
+	    	console.log(obj);
+
+        } catch (ex) {
+            console.log("error in payload - no json object found");
+            console.log("received payload: " + payload);
+        }
+
     });
+    res.status(202).json({}); // Returning empty body & 202 in order to keep payloads on the DASS
+});
+
+
+
+
+// POST managament from DASS 
+appServer.post('/*', function (req, res) {
+
+    log("--------------");
+    log("Received Message posted on URL : " + req.url);
+    
+    req.setEncoding('utf8');
+
+    var payload = '';
+    req.on('data', function (chunk) {
+        payload += chunk;
+    });
+
+    // When the message body is available we can do the query.
+    req.on('end', function () {
+        try {
+            var obj = JSON.parse(payload);
+            if (obj.dataFrame) {
+                obj.dataFrame = new Buffer(obj.dataFrame, 'Base64').toString("hex");
+            }
+            if (obj.timestamp) {
+                obj.timestamp = new Date(obj.timestamp).toJSON();
+            }
+
+            if (obj.gtw_json) {
+                obj.gtw_json = JSON.parse(obj.gtw_json);
+            }
+
+            obj.type = req.url.replace("/rest/callback/", "");
+                
+			//Logging out the payload object
+			console.log(obj);
+
+
+        } catch (ex) {
+            log("error in payload - no json object found");
+            log(payload);
+        }
+    });
+     
+    res.status(202).json({"id": 123}); // Returning empty body & 202 in order to keep payloads on the DASS   
+});
+
+
+function commandInput() {
+    console.log("Enter command... [0-exit, 1-post data]".green)
+    prompt.start();
+    prompt.get(['f','m'], function (err, result) {
+        switch (result.f) {
+            case '0':
+                process.exit();
+                break;
+            case '1':
+                message = new Buffer(result.m, 'hex').toString("base64");
+                host = 'http://localhost:3030/';
+                id = 1;
+                console.log("-------------------------------------------------------------".blue);
+                console.log("SENDING HTTP POST".blue);
+                console.log("HOST:    ".blue + host);
+                console.log("ID:      ".blue + id);
+                console.log("MESSAGE: ".blue + message)
+                httpRequest.post(
+                    host,
+                    {json: {"id": id,                        
+                            "dataFrame": message
+                    }},
+                    function (err, res, body){
+                        if (!err){
+                            console.log("HTTP POST RESPONSE ".red);
+                            console.log("RESPONSE CODE: ".red + res.statusCode);
+                            console.log("RESPONSE BODY: ".red + body.message);
+                            console.log("-------------------------------------------------------------".red);
+                        }
+                    })
+            default:
+                setTimeout(function(){commandInput()}, 2000);
+                
+        }
+        
+    })
+
+}
+
+setTimeout(function(){commandInput()}, 1500);
+
+
