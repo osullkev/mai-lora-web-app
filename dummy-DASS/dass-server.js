893c04c56bf0b8445fd77244fd5c7cc2fbf60fae
Kevin O Sullivan <osullkev@gmail.com>
2018-02-26 18:44:30 +0000

    Small clean up of dummy dass server.

--- dummy-DASS/dass-server.js
+++ dummy-DASS/dass-server.js
@@ -45,8 +45,6 @@ var appSrvrHost = args[3];
 
 var http = require('http');
 var express = require('express');
-var basicAuth = require('basic-auth');
-//var rest = require('./doRest.js');
 var u = require('url');
 var prompt = require('prompt');
 var httpRequest = require('request');
@@ -69,63 +67,7 @@ var httpServer = http.createServer(appServer).listen(appSrvrPort, function () {
     console.log('Starting server listening on port ' + port);
 });
 
-
-
-// PUT management from DASS
-appServer.put('/*', function (req, res) {
-
-    log("--------------");
-    log("Received Message put on URL : " + req.url);
-    var url = u.parse(req.url);
-    log(url);
-
-    req.setEncoding('utf8');
-
-    var payload = '';
-    req.on('data', function (chunk) {
-        payload += chunk;
-    });
-
-    // When the message body is available we can do the query.
-    req.on('end', function () {
-        try {
-            var obj = JSON.parse(payload);
-            if (obj.dataFrame) {
-                obj.dataFrame = new Buffer(obj.dataFrame, 'Base64').toString("hex");
-            }
-
-            if (obj.timestamp) {
-                if (obj.timestamp.toUpperCase().search("Z") >= 0) {
-                    obj.timestamp = new Date(obj.timestamp).toJSON();
-                } else {
-                    obj.timestamp = new Date(obj.timestamp + "Z").toJSON();
-                }
-            }
-
-
-            if (obj.gtw_json) {
-                obj.gtw_json = JSON.parse(obj.gtw_json);
-            }
-
-            obj.type = req.url.replace("/rest/callback/", "");
-            log(obj);
-				
-     		//Logging out the payload object
-	    	console.log(obj);
-
-        } catch (ex) {
-            console.log("error in payload - no json object found");
-            console.log("received payload: " + payload);
-        }
-
-    });
-    res.status(202).json({}); // Returning empty body & 202 in order to keep payloads on the DASS
-});
-
-
-
-
-// POST managament from DASS 
+// POST managament from DASS
 appServer.post('/*', function (req, res) {
 
     console.log("-------------------------------------------------------------".magenta);
@@ -190,7 +132,7 @@ function commandInput() {
                             console.log("RESPONSE BODY: ".red + body.message);
                             console.log("-------------------------------------------------------------".red);
                         }
-                    })
+                    });
             default:
                 setTimeout(function(){commandInput()}, 2000);
                 
