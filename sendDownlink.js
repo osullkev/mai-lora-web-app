10fbf75437110b611fbbd36127c9f3ef7582d35e
Kevin O Sullivan <osullkev@gmail.com>
2018-02-27 16:44:37 +0000

    Namespace devoted to sending POST messages through orbiwise to node.

---
+++ sendDownlink.js
@@ -0,0 +1,12 @@
+var rest = require('./doRest.js');
+var orbiwiseConfig = require('./orbiwise-config.js');
+var nodeConfig = require('./nodeConfig.js');
+
+
+exports.sendDownlink = function (postData) {
+    var dlFcnt = nodeConfig.getdownlinkFrameCount();
+    rest.doRest("POST", "/rest/nodes/0004a30b001b0af1/payloads/dl?fcnt="+dlFcnt+"&port=01", postData, function (status, m) {
+
+    }, orbiwiseConfig.config());
+    nodeConfig.incrementDownlinkFrameCount();
+}
