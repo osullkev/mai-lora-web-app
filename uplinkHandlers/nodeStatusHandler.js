48bb7aa018ddcd192048c9d04fa9d3af50d0902a
Kevin O Sullivan <osullkev@gmail.com>
2018-02-28 12:31:35 +0000

    Node status handler will respond with a update available notification.

--- uplinkHandlers/nodeStatusHandler.js
+++ uplinkHandlers/nodeStatusHandler.js
@@ -2,13 +2,32 @@ require('colors');
 var downlink = require('../sendDownlink.js');
 var utils = require('../utilityFunctions.js');
 
-var retrieveLatestFW = function () {
-    return {'major': 1, 'minor': 5, 'patch': 7};
+
+var assembleFWJSON = function (fwversion) {
+    var major = fwversion.substring(0, 2);
+    var minor = fwversion.substring(2, 4);
+    var patch = fwversion.substring(4);
+    var fwStr = major + "." + minor + "." + patch;
+    return {'major': major, 'minor': minor, 'patch': patch, 'fw_num': fwversion,'fw_string': fwStr};
 }
 
-var sendUpdateAvailabeNotification = function (fwStr, latestFWStr) {
-    console.log("NEW FIRMWARE AVAILABLE: ".red + latestFWStr.major + "." + latestFWStr.minor + "." + latestFWStr.patch);
+var retrieveLatestFW = function () {
+    return assembleFWJSON("010507");
+}
 
+
+var prepareFWUpdateDelta = function (currentFW, newFW) {
+    console.log("Preparing update delta: ".yellow + currentFW.fw_string + " -> ".yellow + newFW.fw_string);
+    console.log("...".yellow);
+    return {'fw_num': newFW.fw_num, 'num_tx_packets': 100, 'CRC': '1234ABCD'};
+}
+
+var sendUpdateAvailableNotification = function (currentFW, latestFW) {
+    console.log("NEW FIRMWARE AVAILABLE: ".red + latestFW.fw_string);
+    var deltaDetails = prepareFWUpdateDelta(currentFW, latestFW);
+    console.log("DELTA:" + deltaDetails)
+    var postData = deltaDetails.fw_num + "1E" + deltaDetails.num_tx_packets + "1E" + deltaDetails.CRC;
+    downlink.sendDownlink(postData);
 }
 
 var pingNode = function(nodeMessage){
@@ -20,7 +39,7 @@ var pingNode = function(nodeMessage){
 
 var checkBatteryLife = function (statusJSON) {
     if (statusJSON.battery_life < 10){
-        console.log("NODE BATTERY IS LOW. REPLACE BATTERY.".red);
+        console.log("NODE BATTERY IS LOW. REPLACE BATTERY: ".red + statusJSON.battery_life +"%");
     }else{
         console.log("NODE BATTERY IS STABLE: ".yellow + statusJSON.battery_life +"%");
     }
@@ -28,21 +47,16 @@ var checkBatteryLife = function (statusJSON) {
 }
 
 var checkFWVersion = function (statusJSON) {
-    var fwversion = statusJSON.fw_version;
-    var major = fwversion.substring(0, 2);
-    var minor = fwversion.substring(2, 4);
-    var patch = fwversion.substring(4);
-    var fwStr = major + "." + minor + "." + patch;
-
+    var currentFWJSON = assembleFWJSON(statusJSON.fw_version);
     var latestFW = retrieveLatestFW();
-    if (latestFW.major > major){
-        sendUpdateAvailabeNotification(fwStr, latestFW);
-    }else if (latestFW.minor > minor){
-        sendUpdateAvailabeNotification(fwStr, latestFW);
-    }else if (latestFW.patch > patch){
-        sendUpdateAvailabeNotification(fwStr, latestFW);
+    if (latestFW.major > currentFWJSON.major){
+        sendUpdateAvailableNotification(currentFWJSON, latestFW);
+    }else if (latestFW.minor > currentFWJSON.minor){
+        sendUpdateAvailableNotification(currentFWJSON, latestFW);
+    }else if (latestFW.patch > currentFWJSON.patch){
+        sendUpdateAvailableNotification(currentFWJSON, latestFW);
     }else {
-        console.log("FIRMWARE VERSION IS UP-TO-DATE: ".yellow + fwStr);
+        console.log("FIRMWARE VERSION IS UP-TO-DATE: ".yellow + currentFWJSON.fw_string);
     }
 }
 
