5f1e8b14d39ebb6f3c4f2a2dc74dc56271c8e748
Kevin O Sullivan <osullkev@gmail.com>
2018-02-28 10:01:48 +0000

    Set downlinks from confirmed=true to confirmed=false

--- sendDownlink.js
+++ sendDownlink.js
@@ -5,7 +5,7 @@ var nodeConfig = require('./nodeConfig.js');
 
 exports.sendDownlink = function (postData) {
     var dlFcnt = nodeConfig.getdownlinkFrameCount();
-    rest.doRest("POST", "/rest/nodes/0004a30b001b0af1/payloads/dl?fcnt="+dlFcnt+"&port=01", postData, function (status, m) {
+    rest.doRest("POST", "/rest/nodes/0004a30b001b0af1/payloads/dl?fcnt="+dlFcnt+"&port=01&confirmed=false", postData, function (status, m) {
 
     }, orbiwiseConfig.config());
     nodeConfig.incrementDownlinkFrameCount();
