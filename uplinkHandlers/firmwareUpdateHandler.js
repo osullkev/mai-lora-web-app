379390cf1d631c8ad2f1eb1ed9e8fd2f60a9c1ce
Kevin O Sullivan <osullkev@gmail.com>
2018-03-07 21:41:59 +0000

    Removed some logging

--- uplinkHandlers/firmwareUpdateHandler.js
+++ uplinkHandlers/firmwareUpdateHandler.js
@@ -4,10 +4,8 @@ var downlink = require('../downlinkHandler.js');
 var updateIndex = 1;
 
 var sendNextUpdatePacket = function(){
-    console.log("Pinging node...");
+    console.log("Sending next firmware update packet...");
     var postData = utils.padWithZeros(updateIndex.toString(), 10);
-    console.log("!!: " + postData)
-
     downlink.sendDownlink('2', postData);
 
     updateIndex++;
