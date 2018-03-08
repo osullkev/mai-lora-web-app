a23975eb7b21d910d678e2b44dee10442da52f06
Kevin O Sullivan <osullkev@gmail.com>
2018-03-08 20:56:57 +0000

    Fixed the opcode in the firmware update packet downlink.

--- uplinkHandlers/firmwareUpdateHandler.js
+++ uplinkHandlers/firmwareUpdateHandler.js
@@ -6,7 +6,7 @@ var updateIndex = 1;
 var sendNextUpdatePacket = function(){
     console.log("Sending next firmware update packet...");
     var postData = utils.padWithZeros(updateIndex.toString(), 10);
-    downlink.sendDownlink('2', postData);
+    downlink.sendDownlink('5', postData);
 
     updateIndex++;
 }
