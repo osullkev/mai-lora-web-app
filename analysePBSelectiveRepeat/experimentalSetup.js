cd51cf67e19e56131050e0fdf367aa179d446fc3
Kevin O Sullivan <osullkev@gmail.com>
2018-04-20 12:35:14 +0100

    Test Analysis

--- analysePBSelectiveRepeat/experimentalSetup.js
+++ analysePBSelectiveRepeat/experimentalSetup.js
@@ -1,5 +1,9 @@
 var path =
-"/home/osullkev/projects/internal/mai-project/mai-lora-web-app/piggybacked-selective-repeat-logs/sf7/1000b/logs_2018-4-16__15-44-31"
+// "/home/osullkev/projects/internal/mai-project/mai-lora-web-app/piggybacked-selective-repeat-logs/sf11/7000b/logs_2018-4-17__23-21-22"
+// "/home/osullkev/projects/internal/mai-project/mai-lora-web-app/piggybacked-selective-repeat-logs/sf12/2000b/logs_2018-4-18__21-15-6"
+// "/home/osullkev/projects/internal/mai-project/mai-lora-web-app/piggybacked-selective-repeat-logs/sf12/3000b/logs_2018-4-19__7-19-1"
+// "/home/osullkev/projects/internal/mai-project/mai-lora-web-app/piggybacked-selective-repeat-logs/sf12/5000b/logs_2018-4-19__16-22-50"
+"/home/osullkev/projects/internal/mai-project/mai-lora-web-app/piggybacked-selective-repeat-logs/sf12/6000b/logs_2018-4-20__3-34-6"
 
 
 var pathArray = path.split("/");
